4abfa21797ef716bebd728370e27d0182a8d7b34
Sarah Rúbia <52300436+sarahrubia@users.noreply.github.com>
2022-09-27 16:03:30 -0300

    feat: add onboarding screen (#1)
    * Create screen components Home, Login and Dashboard
    
    * Remove App.tsx file
    
    * Import Home.tsx to index.js
    
    * Add content and styles to Home screen
    
    * style: move Home styles to a external file
    
    * fix: change image import using require
    
    * remove: remove package-lock.json to avoid conflict
    
    * fix: revert App.tsx deletion
    
    * fix(formatter): fix ESLint and Prettier conflict
    
    * refactor: move Login.tsx and styles to a folder
    
    * feat: add StatusBar to Home.tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
