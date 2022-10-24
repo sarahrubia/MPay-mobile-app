fb9b9c5eb32a53aa60c8aecb96b17b5a37b51b5a
Sarah Rúbia <sarah.rubia@magnetis.com.br>
2022-10-24 18:52:42 -0300

    fix: add type do token

--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -1,7 +1,7 @@
 export const AUTHENTICATE = 'AUTHENTICATE';
 export const LOGOUT = 'LOGOUT';
 
-export function authenticate(token) {
+export function authenticate(token: string) {
   return {
     type: AUTHENTICATE,
     payload: token,
